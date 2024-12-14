c94d2eeeb4bf905640c553509c279fffd6f20c9d
asztaloszoli <asztaloszoli.zo1@gmail.com>
2024-12-14 17:24:34 +0100

    Ruby konstansok frissítése a magyar nyelv támogatásához
    Változtatások:
    - Magyar nyelvi kód hozzáadása az ENABLED_LANGUAGES listához
    - Magyar nyelv beállítása alapértelmezettként

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -4,8 +4,8 @@ module Ladb::OpenCutList
     EXTENSION_VERSION = '6.3.0'.freeze
     EXTENSION_BUILD = '202410151237'.freeze
 
-    DEFAULT_LANGUAGE = 'en'
-    ENABLED_LANGUAGES = %w[ar cs de en es fr he it nl pt ru uk zh vi]
+    DEFAULT_LANGUAGE = 'hu'
+    ENABLED_LANGUAGES = %w[ar cs de en es fr he hu it nl pt ru uk zh vi]
 
     FILE_FORMAT_SKP = 'skp'.freeze
     FILE_FORMAT_STL = 'stl'.freeze
